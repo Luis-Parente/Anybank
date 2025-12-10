aea37f7596731201b7ceba069dbe0ba98520dff9
Luis Parente <parente.lgsp@outlook.com>
2025-12-10 08:37:29 -0300

    adicionado formulario de nova transação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import {BannerComponent} from './componentes/banner-component/banner-component';
+import {FormNovaTransacaoComponent} from './componentes/form-nova-transacao-component/form-nova-transacao-component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    BannerComponent
+    BannerComponent,
+    FormNovaTransacaoComponent
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ src/app/componentes/form-nova-transacao-component/form-nova-transacao-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-nova-transacao-component',
+  imports: [],
+  templateUrl: './form-nova-transacao-component.html',
+  styleUrl: './form-nova-transacao-component.css',
+})
+export class FormNovaTransacaoComponent {
+
+}
